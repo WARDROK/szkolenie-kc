c4a36a42b93100caca446a84bc4271bebbed6a63
Jakub Dziopa <jakub.dziopa@gmail.com>
2026-03-01 12:52:01 +0100

    api route hotfix

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || '/api',
+  baseURL: 'https://backend-kwm-faf8fxg5ckewb2b6.northeurope-01.azurewebsites.net/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
